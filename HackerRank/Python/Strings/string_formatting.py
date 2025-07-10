d2d819bda23e49e17af514419f180e7a857db843
Prathamesh Chikkali <chikkaliprathamesh3@gmail.com>
2025-07-10 18:01:55 +0530

    feat: Solved string formatting problem

---
+++ HackerRank/Python/Strings/string_formatting.py
@@ -0,0 +1,22 @@
+'''Problem Discription : I am provided a number, and I have to print the decimal value, binary value, hexadecimal value and Octal value
+                         of each in range 1 to number. The printed o/p should be in a specific format, like each column is left padded
+                         link : https://www.hackerrank.com/challenges/python-string-formatting
+                         
+Approach : I will use the built in functions like bin() oct() and hex() for the values and will use ljust to obtain the specified
+           print format of left padding'''
+
+def print_formatted(number):
+    # your code goes here
+    x = len(str(bin(number)))-2
+    for i in range(1, number + 1):            #O(nlogn)
+        dec = str(i).rjust(x)
+        octal = oct(i)[2:].rjust(x)
+        hexa = hex(i)[2:].upper().rjust(x)
+        binary = bin(i)[2:].rjust(x)
+        print(f"{dec} {octal} {hexa} {binary}")
+
+
+n = int(input())
+print_formatted(n)
+
+#Time Complexity : O(nlogn)
