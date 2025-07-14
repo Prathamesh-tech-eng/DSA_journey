82323ec946bad6f87c4f554f41e32ab1d1f7348f
Prathamesh Chikkali <chikkaliprathamesh3@gmail.com>
2025-07-14 23:27:04 +0530

    feat: Solved Mod Divmod problem

---
+++ HackerRank/Python/Math/Mod_divmod.py
@@ -0,0 +1,12 @@
+'''Problem Description : I am supposed to take two input a and b and print a//b, a%b and divmod(a, b)
+                         Link : https://www.hackerrank.com/challenges/python-mod-divmod
+                         
+                         '''
+
+A = int(input())
+B = int(input())
+
+print(f"{A//B}\n{A%B}\n{divmod(A,B)}")
+
+#Tme complexity : O(1)
+#Space Complexity : O(1)
