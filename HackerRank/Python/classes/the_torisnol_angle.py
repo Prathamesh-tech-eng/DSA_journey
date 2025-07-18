6e7edd4f8ef0a2d78089e2cb837e7571561cbabb
Prathamesh Chikkali <chikkaliprathamesh3@gmail.com>
2025-07-18 18:47:25 +0530

    feat: Solved finding the torisonal angle problem

---
+++ HackerRank/Python/classes/the_torisnol_angle.py
@@ -0,0 +1,47 @@
+'''Problem Description : I am provided with four points, P,Q,R,S and I need to find the angle between the plane made by the points 
+                         P,Q,R and Q,R,S. 
+                          
+                         Link : https://www.hackerrank.com/challenges/class-2-find-the-torsional-angle
+                          
+ '''
+
+import math
+
+class Points(object):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __sub__(self, no):
+        return Points(self.x - no.x, self.y - no.y, self.z - no.z)
+
+    def dot(self, no):
+        return self.x * no.x + self.y * no.y + self.z * no.z
+
+    def cross(self, no):
+        return Points(
+            self.y * no.z - self.z * no.y,
+            self.z * no.x - self.x * no.z,
+            self.x * no.y - self.y * no.x
+        )
+
+    def absolute(self):
+        return math.sqrt(self.x**2 + self.y**2 + self.z**2)
+
+if __name__ == '__main__':
+    points = list()
+    for i in range(4):
+        a = list(map(float, input().split()))
+        points.append(a)
+
+    a, b, c, d = Points(*points[0]), Points(*points[1]), Points(*points[2]), Points(*points[3])
+    x = (b - a).cross(c - b)
+    y = (c - b).cross(d - c)
+    angle = math.acos(x.dot(y) / (x.absolute() * y.absolute()))
+
+    print("%.2f" % math.degrees(angle))
+
+
+    #Time Complexity : O(1)
+    #Space Complexity : O(1)
